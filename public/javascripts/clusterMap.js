d589eb0cb8615d88d88baacac9e194288fe15e13
anan <nut999anan@gmail.com>
2022-01-20 17:06:16 +0700

    done with styling

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,11 +2,13 @@ console.log(campgrounds[0]);
 
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: "map",
+  container: "cluster-map",
   style: "mapbox://styles/mapbox/dark-v10",
   center: [-103.5917, 40.6699],
   zoom: 3,
 });
+
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-left');
 
 map.on("load", () => {
   // Add a new source from our GeoJSON data and
